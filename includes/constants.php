3592838dc4c5a66f1ec5079449150a19f2d89243
Your Name <Polomints1@gmail.com>
2011-12-05 22:55:13 +0000

    adding entire project

---
+++ includes/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+// Define constants here
+
+define('DB_SERVER', '10.194.111.8');
+define('DB_USER', 'user_ff7ae43c');
+define('DB_PASSWORD', 'GOmhtmFtnyV4ZY');
+define('DB_NAME', 'db_ff7ae43c');
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+ 
+require_once 'classes/Membership.php';
+$membership = New Membership();
+ 
+$membership->confirm_Member();
+ 
+?>
+ 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/default.css" />
+ 
+<!--[if lt IE 7]>
+<script type="text/javascript" src="js/DD_belatedPNG_0.0.7a-min.js"></script>
+<![endif]-->
+ 
+ 
+<title>Untitled Document</title>
+ 
+ 
+ 
+</head>
+ 
+<body>
+ 
+<div id="container">
+<p>
+ 
+</p>
+<a href="login.php?status=loggedout">Log Out</a>
+</div><!--end container-->
+ 
+</body>
+</html>
